e64e1f43acfa6196d45158950b3ee9928e43e07b
Poornashree19 <poornashree.2201155@srec.ac.in>
2024-07-01 16:37:44 +0530

    passengerlatlng uploaded

---
+++ models/passengerlatlng.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const LatlngSchema = new mongoose.Schema({
+    pickupCoordinates: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true,
+            validate: {
+                validator: function(coords) {
+                    return coords.length === 2 &&
+                           coords[0] >= -180 && coords[0] <= 180 &&  
+                           coords[1] >= -90 && coords[1] <= 90;     
+                },
+                message: props => `${props.value} is not a valid coordinate pair.`
+            }
+        }
+    },
+    address: {
+        type: String,
+        required: false
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Passengerlatlng = mongoose.model("Passengerlatlng", LatlngSchema);
+module.exports=Passengerlatlng;
